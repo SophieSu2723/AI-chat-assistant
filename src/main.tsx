529ba3c4c98b24d4e73ab3e051e42972e27b8d6c
SophieSu2723 <fs495@cornell.edu>
2026-09-17 05:19:07 +0800

    new

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import './styles.css'
+import './import.css'
 import { App } from './App'
 
 // Check for a new version whenever the app is reopened.  The update is applied
